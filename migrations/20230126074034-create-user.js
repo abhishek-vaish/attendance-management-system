7388bf1bc338338d7b675961d14d1027a18a116c
abhishekv5055 <abhishekv5055@gmail.com>
2023-01-26 17:09:31 +0530

    initialize sequelize

---
+++ migrations/20230126074034-create-user.js
@@ -0,0 +1,53 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    queryInterface.createTable("Users", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      department: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "departments",
+          key: "id",
+        },
+      },
+      position: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "positions",
+          key: "id",
+        },
+      },
+      start_time: {
+        type: Sequelize.TIME,
+      },
+      end_time: {
+        type: Sequelize.TIME,
+      },
+      report_manager: {
+        type: Sequelize.INTEGER,
+      },
+      project_manager: {
+        type: Sequelize.INTEGER,
+      },
+      is_active: {
+        type: Sequelize.BOOLEAN,
+      },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    queryInterface.dropTable("Users");
+  },
+};
---
+++ migrations/20230126111558-create-leave.js
@@ -0,0 +1,53 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    queryInterface.createTable("Leave", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      user: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+      },
+      leave_start_date: {
+        type: Sequelize.DATE,
+      },
+      leave_end_date: {
+        type: Sequelize.DATE,
+      },
+      leave_type: {
+        type: Sequelize.ENUM(
+          "Standard",
+          "Earned",
+          "Bereavement",
+          "Leave Without Pay",
+          "Optional Holiday",
+          "Paternity",
+          "Sabbatical"
+        ),
+      },
+      leave_reason: {
+        type: Sequelize.STRING,
+      },
+      approvals: {
+        type: Sequelize.BOOLEAN,
+      },
+      approval_date: {
+        type: Sequelize.DATE,
+      },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    queryInterface.dropTable("Leave");
+  },
+};
