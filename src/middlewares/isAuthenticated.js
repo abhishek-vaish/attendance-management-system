d5c874af9d8b6c24a43704f6334702af25f94eea
abhishekv5055 <abhishekv5055@gmail.com>
2023-01-30 17:47:58 +0530

    add getUser middleware

--- src/middlewares/isAuthenticated.js
+++ src/middlewares/isAuthenticated.js
@@ -1,3 +1,5 @@
+const Token = require("../models/Token.model");
+
 exports.isAuthenticated = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
@@ -5,5 +7,21 @@ exports.isAuthenticated = (req, res, next) => {
     res.status(401).json({ message: "Unauthorized" });
   }
   req.token = token;
+  next();
+};
+
+exports.getUser = async (req, res, next) => {
+  const token = req.token;
+  const authenticatedUser = await Token.findOne({
+    where: { token: token },
+    attributes: ["Authentication.user"],
+    include: { model: "Authentication" },
+  });
+
+  if (authenticatedUser === null) {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+
+  req.user = authenticatedUser.toJSON()["Authentication"]["user"];
   next();
 };
