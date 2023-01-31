573d6b6e75ccc93a4bbedc032653efd3b54de09c
abhishekv5055 <abhishekv5055@gmail.com>
2023-01-31 15:32:26 +0530

    create function for latest date and time

--- src/controllers/punchtime.controller.js
+++ src/controllers/punchtime.controller.js
@@ -1,35 +1,14 @@
-const { Op, where } = require("sequelize");
+const { Op } = require("sequelize");
 const PunchTimes = require("../models/punchtime.model");
-
-const currentDate = new Date();
-const currTime =
-  (currentDate.getHours() < 10 ? "0" : "") +
-  currentDate.getHours() +
-  ":" +
-  (currentDate.getMinutes() < 10 ? "0" : "") +
-  currentDate.getMinutes() +
-  ":" +
-  (currentDate.getSeconds() < 10 ? "0" : "") +
-  currentDate.getSeconds();
-
-const currDate =
-  (currentDate.getFullYear() < 10 ? "0" : "") +
-  currentDate.getFullYear() +
-  "-" +
-  (currentDate.getMonth() < 9 ? "0" : "") +
-  (currentDate.getMonth() + 1) +
-  "-" +
-  (currentDate.getDate() < 10 ? "0" : "") +
-  currentDate.getDate();
+const { currTime, currDate } = require("../resources/getDate");
+const moment = require("moment/moment");
 
 exports.punchStart = async (req, res) => {
   const user = req.user;
 
   const ptRecord = await PunchTimes.findOne({
-    where: { [Op.and]: { user: user, date: currDate } },
+    where: { [Op.and]: { user: user, date: currDate() } },
   });
-
-  console.log(ptRecord.id);
 
   if (ptRecord !== null) {
     res.status(400).json({ message: "You already punch your timings" });
@@ -37,9 +16,9 @@ exports.punchStart = async (req, res) => {
     try {
       await PunchTimes.create({
         user: user,
-        punch_start_time: currTime,
+        punch_start_time: currTime(),
         punch_end_time: null,
-        date: currDate,
+        date: currDate(),
       });
       res.status(201).json({ success: "Ok" });
     } catch (err) {
@@ -52,7 +31,7 @@ exports.punchEnd = async (req, res) => {
   const user = req.user;
 
   const ptRecord = await PunchTimes.findOne({
-    where: { [Op.and]: { user: user, date: currDate } },
+    where: { [Op.and]: { user: user, date: currDate() } },
   });
 
   if (ptRecord === null) {
@@ -60,9 +39,9 @@ exports.punchEnd = async (req, res) => {
   } else {
     try {
       await PunchTimes.update(
-        { punch_end_time: currTime },
+        { punch_end_time: currTime() },
         {
-          where: { [Op.and]: { user: user, date: currDate } },
+          where: { [Op.and]: { user: user, date: currDate() } },
         }
       );
       res.status(200).json({ success: "Ok" });
