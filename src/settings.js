ea5676d643dc3eb462011af5f044a762cfda6b5e
abhishekv5055 <abhishekv5055@gmail.com>
2023-02-01 11:33:57 +0530

    clean code with import and export

---
+++ src/settings.js
@@ -0,0 +1,12 @@
+import fs from "fs";
+import path from "path";
+
+export const BASE_DIR = path.dirname(__dirname);
+export const ENVIRONMENT = process.env.AMS_ENV || "development";
+export const CONFIG = JSON.parse(
+  fs.readFileSync(
+    path.join(BASE_DIR, "resources", `${ENVIRONMENT}.json`),
+    "utf-8"
+  )
+);
+export default { ENVIRONMENT: CONFIG.DATABASE };
